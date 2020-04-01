b2be5569a52d34619da873d8d7f2a9b1bb62e907
Linder <vhelyno@gmail.com>
2020-04-01 16:53:17 +0200

    server http en python

--- http/main.py
+++ http/main.py
@@ -1,1 +1,10 @@
-print('hello')
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
