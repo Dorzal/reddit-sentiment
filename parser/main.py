550e9c4320acb7036deb64e5bdf13f87a2b36606
hschweitzer <hugo.schweitzer@viaesi.fr>
2020-04-02 14:23:15 +0200

    tempo ajout textblob + connexion db

--- parser/main.py
+++ parser/main.py
@@ -1,1 +1,22 @@
-print('hello')
+from textblob import TextBlob
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://sentiment:iYZQsvRbKy9SdXnx@python-9sotq.mongodb.net/test")
+db = client.sentiments
+
+polarite = client.polarite
+
+blob = TextBlob("hello")
+
+# if blob.detect_language() != 'en':
+#     blob_en = blob.translate(to='en')
+# else:
+#     blob_en = blob
+blob_en = blob
+
+blob_en.tags
+blob_en.noun_phrases
+
+for sentence in blob_en.sentences:
+    if (sentence.sentiment.polarity != 0):
+        print(sentence)
+        print(sentence.sentiment.polarity)
