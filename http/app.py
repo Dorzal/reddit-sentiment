8898b96ddc5d3a1a0cdbd85981cec879935887ce
Linder <vhelyno@gmail.com>
2020-04-02 14:22:05 +0200

    route api/data

--- http/app.py
+++ http/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 import DataService 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return DataService.retrieveData()
+@app.route('/api/data')
+def getData():
+    subject = request.args.get('subject')
+    if(subject):
+        return subject
+    else:
+        return 'rien'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
