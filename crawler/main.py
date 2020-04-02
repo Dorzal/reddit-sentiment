6743272042192b2c180a2ad940d5654ca1ea2182
Nico.L <nicolas.lardier@viacesi.fr>
2020-04-02 16:04:55 +0200

    Ajout des requirement pour le crawler + Modif des données JSON à envoyer dans la bdd

--- crawler/main.py
+++ crawler/main.py
@@ -1,6 +1,6 @@
 from  pymongo import MongoClient
 import praw
-from datetime import date
+from datetime import datetime
 
 #Connexion à la BDD
 client = MongoClient("mongodb+srv://sentiment:iYZQsvRbKy9SdXnx@python-9sotq.mongodb.net/test")
@@ -11,13 +11,18 @@ collection = db["crawl"]
 reddit = praw.Reddit(client_id ='fEQd43i9zHyhqA', client_secret='LpLwmKtOuUmrfdqhrlnYt8znvk4', user_agent='ProjetTendanceV1')
 
 #Catégorie de récupération
-subreddit = reddit.subreddit('python')
-hot_topic = subreddit.hot(limit=2)
+subreddit = reddit.subreddit('news')
+hot_topic = subreddit.hot(limit=1)
 
 #Récupération des infos
-for post in hot_topic:    
+for post in hot_topic:
+    #On récupère la liste des commentaires
     comments = post.comments.list()
     for comment in comments:
-        if comment.score > 1:
-            jsonStr = {"title": post.title, "contenu": post.selftext, "reaction": comment.body, "date": format(date.today()), "score": post.score}
+        #if isinstance(comment, MoreComments):
+        if comment.score > 10:
+            #On formate la date    
+            dateCom = datetime.fromtimestamp(comment.created)
+            jsonStr = {"title": post.title, "contenu": post.selftext, "reaction": comment.body, "date": format(dateCom), "score": comment.score, "social_network": 'REDDIT'}
+            #On insère la donnée dans la BDD
             collection.insert_one(jsonStr)
