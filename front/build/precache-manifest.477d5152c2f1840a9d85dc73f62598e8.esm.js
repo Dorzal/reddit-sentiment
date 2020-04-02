c54121a4646f5d8629339842b0309c228999cdb9
Jean-Christophe Pires <jeanchristophe.pires55@gmail.com>
2020-04-02 14:12:19 +0200

    Push fichiers de build

---
+++ front/build/precache-manifest.477d5152c2f1840a9d85dc73f62598e8.esm.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "98371cc52b8abf56f803",
+    "url": "/bundle.33c5a.css"
+  },
+  {
+    "revision": "98371cc52b8abf56f803",
+    "url": "/bundle.bab96.esm.js"
+  },
+  {
+    "revision": "283439dc779d1cb31597fc79152eea97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ea856636118a96855c",
+    "url": "/polyfills.10800.esm.js"
+  }
+]);
