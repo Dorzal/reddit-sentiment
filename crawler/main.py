e22cd9b610df12b4a9643e9a16ef44af95d6cc90
Nico.L <nicolas.lardier@viacesi.fr>
2020-04-02 10:23:26 +0200

    Connexion a la bdd ok + test insertion une ligne ok

--- crawler/main.py
+++ crawler/main.py
@@ -1,28 +1,30 @@
-#from pymongo import MongoClient
-#from pprint import pprint
+"""from  pymongo import MongoClient
+from pprint import pprint
 
-#client = MongoClient()
+client = MongoClient("mongodb+srv://sentiment:iYZQsvRbKy9SdXnx@python-9sotq.mongodb.net/test")
 
-#db = client.admin
-#serverStatusResult = db.command("serverStatus")
-#pprint(serverStatusResult)
+db = client["sentiments"]
+collection = db["crawl"]
+
+post = {"id": 0, "name": "nico", "score": 5}
+collection.insert_one(post)"""
+
 import praw
 
-reddit = praw.Reddit(client_id ='fEQd43i9zHyhqA', client_secret='LpLwmKtOuUmrfdqhrlnYt8znvk4', username='W4rNoX', password='Cookies54', user_agent='ProjetTendanceV1')
+reddit = praw.Reddit(client_id ='fEQd43i9zHyhqA', client_secret='LpLwmKtOuUmrfdqhrlnYt8znvk4', user_agent='ProjetTendanceV1')
 
-subreddit = reddit.subreddit('popular')
+subreddit = reddit.subreddit('python')
 
-hot_topic = subreddit.hot(limit=2)
+hot_topic = subreddit.hot(limit=5)
 
-for submission in hot_topic:
-    if not submission.stickied:
-        print()
-        print('Titre: {}, Ups: {}, Downs : {}'.format(submission.title, submission.ups, submission.downs))
-        print()
+for post in hot_topic:
+    print()
+    print('Titre: {}, Ups: {}, Downs : {}, Is video : {}'.format(post.title, post.ups, post.downs, post.selftext))
+    print()
     
-    """comments = submission.comments.list()
+    comments = submission.comments.list()
     for comment in comments:
         print(30*'-')
         print('Parent ID : ', comment.parent())
         print('Comment ID : ', comment.id)
-        print(comment.body)"""
+        print(comment.body)
