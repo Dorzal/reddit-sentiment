abfe254b29f4fa35e7fe39db6f7b6ae6b32c06ef
Linder <vhelyno@gmail.com>
2020-04-03 10:20:20 +0200

    wip retour api

--- http/DataService.py
+++ http/DataService.py
@@ -1,31 +1,31 @@
+# coding= utf-8
+
 from pymongo import MongoClient
 import re
-import request
+import requests
+import json
 
-#recupére les datas ou appel le crawler
+
 def retrieveData(subject):
 
-    #connexion à la base de données mongo
+
     client = MongoClient("mongodb+srv://sentiment:iYZQsvRbKy9SdXnx@python-9sotq.mongodb.net/test")
     db = client["sentiments"]
     collection = db["polarite"]
 
-    #regex like 
-    rgx = re.compile('.*'subject'.*', re.IGNORECASE)
+    rgx = re.compile('.*'+subject+'.*', re.IGNORECASE)
 
     myquery = { "sujet": rgx }
 
-    #recherche en fonction de la requete 
-    mydoc = mycol.find(myquery)
+    mydoc = collection.find(myquery)
 
-    # si retour alors renvoi la collection sinon appel crawler
     if(mydoc):
-        return mydoc
+        for x in mydoc:
+            return x
     else:
         callCrawler(subject)
         return 'loader'
 
-#appel crawler via POST
 def callCrawler(subject):
     url = "http://crawler_by_id:5000"
     data = {"search": subject}
