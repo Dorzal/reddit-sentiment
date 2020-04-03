d4e774f44a89ed3a11be11cb562c9ae6fe2131c5
hschweitzer <hugo.schweitzer@viaesi.fr>
2020-04-03 10:58:48 +0200

    ajout calcul et insertion des polarités

--- parser/main.py
+++ parser/main.py
@@ -2,10 +2,14 @@ from textblob import TextBlob
 from pymongo import MongoClient
 from pprint import pprint
 from bson.son import SON
+import datetime
 import json
 
-client = MongoClient('mongodb+srv://sentiment:iYZQsvRbKy9SdXnx@python-9sotq.mongodb.net/test?authSource=admin&replicaSet=python-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
-result = client['sentiments']['crawl'].aggregate([
+client = MongoClient('mongodb+srv://sentiment:iYZQsvRbKy9SdXnx@python-9sotq.mongodb.net/test')
+crawl = client['sentiments']['crawl']
+polarite = client['sentiments']['polarite']
+
+listPosts = crawl.aggregate([
     {
         '$group': {
             '_id': {
@@ -27,7 +31,7 @@ result = client['sentiments']['crawl'].aggregate([
         }
     }, {
         '$group': {
-            '_id': '$_id.title', 
+            '_id': '$_id.title',
             'comments': {
                 '$push': '$$ROOT'
             }
@@ -35,21 +39,47 @@ result = client['sentiments']['crawl'].aggregate([
     }
 ])
 
-pprint(list(result))
+# pprint(list(listPosts))
 
-# res = {}
-# for post in listPosts:
-#     for comment in post:
-#         blob = TextBlob(comment)
-#         for sentence in blob.sentences:
-#             print()
+res = {}
+for post in listPosts:
+    title = post['_id']
+    res[title] = {}
 
-# blob = TextBlob("hello")
+    for commentHour in post['comments']:
+        date = commentHour['comments'][0]['date']
+        commentDate = date.replace(minute=0, second=0, microsecond=0)
+        res[title][commentDate] = {}
+        avgPolarite = 0
 
-# blob.tags
-# blob.noun_phrases
+        for comment in commentHour['comments']:
+            text = comment['reaction']
+            blob = TextBlob(text)
+            blob.tags
+            blob.noun_phrases
 
-# for sentence in blob.sentences:
-#     if (sentence.sentiment.polarity != 0):
-#         print(sentence)
-#         print(sentence.sentiment.polarity)
+            for sentence in blob.sentences:
+                avgPolarite += sentence.sentiment.polarity
+                avgPolarite = avgPolarite / 2
+            
+            jsonStrDelete = {
+                'title': title,
+                'reaction': text,
+                'date': date
+            }
+            existing_crawl = crawl.find(jsonStrDelete)
+            if existing_crawl:
+                crawl.delete_many(jsonStrDelete)
+
+        jsonStrInsert = {
+            "title":  title,
+            "date": commentDate,
+            "social_network": 'REDDIT',
+            "polarite": round(avgPolarite, 2)
+        }
+        existing_polarite = polarite.find_one(jsonStrInsert)
+
+        if not existing_polarite:
+            polarite.insert_one(jsonStrInsert)
+        else:
+            polarite.update_one(existing_polarite, {'$set': jsonStrInsert})
