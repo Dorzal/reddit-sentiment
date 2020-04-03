612be931bbb675cfffd1baf99dd2b610ed1df184
Jean-Christophe Pires <jeanchristophe.pires55@gmail.com>
2020-04-03 10:38:57 +0200

    wip front v2

--- front/src/components/organisms/statistiques/graphique.js
+++ front/src/components/organisms/statistiques/graphique.js
@@ -1,49 +1,15 @@
 import { useState, useEffect } from 'preact/hooks';
 import { Line } from 'react-chartjs-3';
+ 
+const Graph = ({
+    results
+}) => {
+    const [datas, SetDatas] = useState(results)
 
-const initialState = {
-    labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Setptembre', 'Octobre', 'Novembre', 'Décembre'],
-    datasets: [
-      {
-          label: 'Evolution des sentiments',
-          backgroundColor: 'rgba(103, 58, 183,0.2)',
-          borderColor: 'rgba(103, 58, 183,1)',
-          borderWidth: 4,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [0.1, 0.3, 0.42, 0.45, 0.6, 0.1, -0.5, -2, 1, 1.5, -0.30, -0.1]
-      }
-    ]
-  };
-  
-const Graph = () => {
-    const [datas, SetDatas] = useState(initialState)
+    useEffect(() => {
+        SetDatas(results);
+    }, [results])
 
-	useEffect(() => {
-        setInterval(function(){
-            console.log("YO")
-			var oldDataSet = datas.datasets[0];
-			var newData = [];
-
-			for(var x=0; x< datas.labels.length; x++){
-				newData.push(Math.random());
-			}
-
-			var newDataSet = {
-				...oldDataSet
-			};
-
-			newDataSet.data = newData;
-
-			var newState = {
-				...initialState,
-				datasets: [newDataSet]
-			};
-
-			SetDatas(newState);
-		}, 5000);
-    }, [])
-	
     return (
         <Line
         data={datas} 
@@ -55,10 +21,12 @@ const Graph = () => {
 };
   
 
-export default () => {
+export default ({
+    results
+}) => {
     return (
         <div style={{height: "75vh"}}>
-            <Graph />
+            <Graph results={results}/>
         </div>
     )
 }
--- front/src/components/templates/content/index.js
+++ front/src/components/templates/content/index.js
@@ -5,19 +5,54 @@ import Formulaires from '../../organisms/formulaires';
 import Titles from '../../atomics/titles';
 import Statistiques from '../../organisms/statistiques';
 
+
+const initialState = {
+    labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Setptembre', 'Octobre', 'Novembre', 'Décembre'],
+    datasets: [
+      {
+          label: 'Evolution des sentiments',
+          backgroundColor: 'rgba(103, 58, 183,0.2)',
+          borderColor: 'rgba(103, 58, 183,1)',
+          borderWidth: 4,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: []
+      }
+    ]
+  };
+
 export default () => {
-	const [results, setResults] = useState(false)
+	const [results, setResults] = useState(initialState)
 	const [inputval, setInputVal] = useState('');
 
 	const rechercher = () => {
-		alert(inputval)
-		setInputVal(inputval)
+		
+		changeGraphiqueDatas([0.1, 0.3, 0.42, 0.45, 0.6, 0.1, -0.5, -2, 1, 1.5, -0.30, -0.1])
+		fetch(`http://localhost:5000/api/data?subject=${inputval}`)
+		.then(response => response.json())
+		.then(json => {
+			//
+		})
+	}
+
+	const changeGraphiqueDatas = (newDatasets) => {
+		var oldDataSet = results.datasets[0];
+		var newData = newDatasets;
+		var newDataSet = {
+			...oldDataSet
+		};
+		newDataSet.data = newData;
+		var newState = {
+			...initialState,
+			datasets: [newDataSet]
+		};
+
+		setResults(newState);
 	}
 
 	return (
 		<div id={style.content}>
-			<div onClick={() => setSearched(false)}>
-				
+			<div>
 				<Formulaires.Recherche
 					inputVal={inputval}
 					onValid={(val) => rechercher(val)}
@@ -25,7 +60,7 @@ export default () => {
 					label={<Titles.H2>Rechercher un sujet</Titles.H2>}
 					style={{formulaire: style.formulaire, input: style.input, label: style.label}}
 				/>
-				<Statistiques.Graphique />
+				<Statistiques.Graphique results={results}/>
 			</div> 
 			
 		</div>
